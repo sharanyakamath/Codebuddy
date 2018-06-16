972f34d0c065e0d81462323adacca0c71a00271c
sharanyakamath <sherrykamath@gmail.com>
2018-06-16 09:56:30 +0530

    update

--- Web_Enthusiast's_Club/Week5/Week5_q1.cpp
+++ Web_Enthusiast's_Club/Week5/Week5_q1.cpp
@@ -5,19 +5,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-	int n,i,j,k,x,sum=INT_MAX;
+	int n,i,j,k,x,sum=INT_MAX,sum1,l,r;
 	cin>>n;
+
 	int A[n];
 	for(i=0;i<n;i++)
 		cin>>A[i];
-	cin>>x;
+
+	cin>>k;
+	sort(A,A+n);
+
 	for(i=0;i<n-2;i++){
-		for(j=i+1;j<n-1;j++){
-			for(k=j+1;k<n;k++){
-				if(abs(x-sum)>abs(x-(A[i]+A[j]+A[k])))
-					sum=A[i]+A[j]+A[k];
+		x=A[i];
+		l=i+1, r=n-1;
+		while(l<r){
+			sum1=x+A[l]+A[r];
+			if(sum1==k){
+				cout<<k<<endl;
+				goto end;
 			}
+			else if(sum1<k)
+				l++;
+			else 
+				r--;
 		}
+		if(abs(k-sum)>abs(k-sum1))
+			sum=sum1;
 	}
+	
 	cout<<sum<<endl;
+	end:;
 }
