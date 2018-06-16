396f3a95459125eb460952a9c6b23249f29ef3a7
sharanyakamath <sherrykamath@gmail.com>
2018-06-16 10:42:20 +0530

    updates

--- Web_Enthusiast's_Club/Week5/Week5_q2.cpp
+++ Web_Enthusiast's_Club/Week5/Week5_q2.cpp
@@ -5,21 +5,29 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+
+void minjumps(int n, int A[]){
+	int maxi=A[0],steps=A[0],jumps=1,i;
+	if(A[0]==0)
+		{cout<<-1<<endl; return;}
+	for(i=1;i<n;i++){
+		maxi=max(maxi,i+A[i]);
+		if(i==n-1)
+			{cout<<jumps<<endl; return;}
+		steps--;
+		if(steps==0){
+			if(i>=maxi)
+				{cout<<-1<<endl; return;}
+			steps=maxi-1;
+			jumps++;
+		}
+	}	
+}
 int main(){
-	int n,i,j,min;
+	int n,i;
 	cin>>n;
-	int A[n],dp[n];
+	int A[n];
 	for(i=0;i<n;i++)
 		cin>>A[i];
-	dp[n-1]=0;
-	for(i=n-2;i>=0;i--){
-		dp[i]=-1, min=INT_MAX;
-		for(j=1;j<=A[i] && i+j<n;j++){
-			if(min>dp[i+j] && dp[i+j]!=-1)
-				min=1+dp[i+j];
-		}
-		if(min!=INT_MAX)
-			dp[i]=min;
-	}
-	cout<<dp[0]<<endl;
+	minjumps(n,A);
 }
