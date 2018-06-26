19ae6cc66177603f8dbc94e3459779d2f443dcd8
Kamath <sharanya.kamath@here.com>
2018-06-26 11:18:19 +0530

    week 7

--- Web_Enthusiast's_Club/Week7/Week7_q1.cpp
+++ Web_Enthusiast's_Club/Week7/Week7_q1.cpp
@@ -1,49 +1,52 @@
+//Given a string S composed of 0 and 1. Find the minimum splits such that the substring is a binary representation of the power of 4 or 6 with no leading zeros. Print -1 if no such partitioning is possible.
+//Sharanya Kamath and Vighnesh V
+
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isPowerOf(long val, int base)
+long long int power(long long int v, long long int base)
 {
-    while (val > 1) {
-        if (val % base != 0)
-            return false; 
-        val /= base;
+    while(v>1){
+        if (v%base!=0)
+            return 0; 
+        v/=base;
     }
-    return true;
+    return 1;
 }
 
-int numberOfPartitions(string binaryNo)
+long long int parts(char num[])
 {
-    int i, j, n = binaryNo.length();
-    long val;
-    int dp[n];
+    long long int i,j,n=strlen(num);
+    long long int v;
+    long long int dp[n];
  
-    dp[n - 1] = ((binaryNo[n - 1] - '0') == 0) ? -1 : 1;
- 
-    for (i = n - 2; i >= 0; i--) {
-        val = 0;
-        if ((binaryNo[i] - '0') == 0) {
-            dp[i] = -1;
+    dp[n-1]=((num[n-1]-'0')==0)?-1:1;
+
+    for (i=n-2;i>=0;i--){
+        v=0;
+        if((num[i]-'0')==0){
+            dp[i]=-1;
             continue;
         }
  
-        dp[i] = INT_MAX;
+        dp[i]=INT_MAX;
  
-        for (j = i; j < n; j++) {
+        for (j=i;j<n;j++){
  
-            val = (val * 2) + (long)(binaryNo[j] - '0');
-            if (isPowerOf(val, 4) || isPowerOf(val, 6)) {
-                if (j == n - 1) {
-                    dp[i] = 1;
+            v=(v*2)+(long long int)(num[j]-'0');
+            if (power(v,4)||power(v,6)) {
+                if (j==n-1){
+                    dp[i]=1;
                 }
                 else {
-                    if (dp[j + 1] != -1)
-                        dp[i] = min(dp[i], dp[j + 1] + 1);
+                    if(dp[j+1]!=-1)
+                        dp[i]=min(dp[i], dp[j+1]+1);
                 }
             }
         }
 
-        if (dp[i] == INT_MAX)
-            dp[i] = -1;
+        if (dp[i]==INT_MAX)
+            dp[i]=-1;
     }
  
     return dp[0];
@@ -51,8 +54,11 @@ int numberOfPartitions(string binaryNo)
 
 int main()
 {
-    string binaryNo;
-    cin>>binaryNo;
-    cout << numberOfPartitions(binaryNo);
-    return 0;
+	int t;
+	cin>>t;
+	while(t--){
+		char num[50];
+    	cin>>num;
+    	cout<<parts(num)<<endl;
+	}
 }
--- Web_Enthusiast's_Club/Week7/Week7_q2.cpp
+++ Web_Enthusiast's_Club/Week7/Week7_q2.cpp
@@ -1,3 +1,7 @@
+//Rahul entered lift on the ground floor of his building which consists of Z floors including the ground floor. The lift already had N people in it. It is known that they will leave the lift in groups. Let us say that there are M groups. Rahul is curious to find the number of ways in which these M groups can leave the lift. It is assumed that each group is unique and no one leaves the lift on the ground floor.
+//Since the answer can be too large take modulo 1000000007.
+//Sharanya Kamath and Vighnesh V
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
